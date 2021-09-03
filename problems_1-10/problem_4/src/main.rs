207cb8eb9b156486995bd8d0b2b51bc722e58e35
Andrea Berardi <Kotheos@protonmail.com>
2021-09-03 23:41:14 +0200

    problem 4

---
+++ problems_1-10/problem_4/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut biggest_palindrome: u32 = 0;
+
+    for a in 0..=999 {
+        for b in 0..=999 {
+            if (a * b).is_palindrome() && a * b > biggest_palindrome {
+                biggest_palindrome = a * b;
+            }
+        }
+    }
+
+    println!("{}", biggest_palindrome);
+}
+
+trait Palindrome {
+    fn is_palindrome(&self) -> bool;
+}
+
+impl Palindrome for u32 {
+    fn is_palindrome(&self) -> bool {
+        let s: Vec<char> = self.to_string().chars().collect();
+        let mut rev_s: Vec<char> = s.clone();
+        rev_s.reverse();
+
+        s == rev_s
+    }
+}
