88fd04cca4a971d513bf5fc6b13ff34520eda852
Andrea Berardi <Kotheos@protonmail.com>
2021-09-12 12:49:29 +0200

    implementis problem 14...

---
+++ problems_11-20/problem_14/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    //let mut n: u64 = 0;
+    //let mut max: u32 = 0;
+//
+    //for number in 0..=1_000_000 {
+    //    n = number;
+    //    let mut count: u32 = 0;
+//
+    //    while n != 1 {
+    //        if n % 2 == 0 {
+    //            n /= 2;
+    //        } else {
+    //            n = 3 * n + 1;
+    //        }
+//
+    //        count += 1;
+    //    }
+//
+    //    if count > max {
+    //        max = count;
+    //    }
+    //}
+//
+    //println!("{}", n);
+}
