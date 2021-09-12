be649cb9bbe5f27763cbdd93e62006539248e08d
Andrea Berardi <Kotheos@protonmail.com>
2021-09-13 00:00:14 +0200

    problem 15 (overflows, not the final version)

---
+++ problems_11-20/problem_15/src/main.rs
@@ -0,0 +1,23 @@
+// this solution is theoretically correct, but the numbers are too big to fit in a unsigned 128bit integer
+fn main() {
+    const GRID: u128 = 20;
+
+    println!("{}", (GRID * 2).factorial() / (GRID.factorial().pow(2)));
+}
+
+trait Factorial {
+    fn factorial(&self) -> u128;
+}
+
+impl Factorial for u128 {
+    fn factorial(&self) -> u128 {
+        let mut factorial: u128 = 1;
+
+        for n in 1..=*self {
+            factorial *= n;
+            println!("{}! = {}", n, factorial);
+        }
+
+        factorial
+    }
+}
