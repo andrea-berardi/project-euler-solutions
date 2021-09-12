f7dfc45482b4659da7a2647bc7cb173ddf435c23
Andrea Berardi <Kotheos@protonmail.com>
2021-09-12 16:54:20 +0200

    problem 14, corrected and working (not super-fast)

--- problems_11-20/problem_14/src/main.rs
+++ problems_11-20/problem_14/src/main.rs
@@ -1,25 +1,30 @@
+// Longest Collatz sequence
+// https://projecteuler.net/problem=14
+
 fn main() {
-    //let mut n: u64 = 0;
-    //let mut max: u32 = 0;
-//
-    //for number in 0..=1_000_000 {
-    //    n = number;
-    //    let mut count: u32 = 0;
-//
-    //    while n != 1 {
-    //        if n % 2 == 0 {
-    //            n /= 2;
-    //        } else {
-    //            n = 3 * n + 1;
-    //        }
-//
-    //        count += 1;
-    //    }
-//
-    //    if count > max {
-    //        max = count;
-    //    }
-    //}
-//
-    //println!("{}", n);
+    let mut _n: u64 = 0;
+    let mut max: u64 = 0;
+    let mut max_n: u64 = 0;
+
+    for number in 1..1_000_000 {
+        _n = number;
+        let mut count: u64 = 0;
+
+        while _n != 1 {
+            if _n % 2 == 0 {
+                _n /= 2;
+            } else {
+                _n = 3 * _n + 1;
+            }
+
+            count += 1;
+        }
+
+        if count > max {
+            max = count;
+            max_n = number;
+        }
+    }
+
+    println!("{}", max_n);
 }
