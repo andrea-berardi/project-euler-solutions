1425b27bf9f2294bf2014558c87933c812d7da54
Andrea Berardi <Kotheos@protonmail.com>
2021-09-13 22:10:10 +0200

    commented the broken code :(

--- problems_11-20/problem_15/src/main.rs
+++ problems_11-20/problem_15/src/main.rs
@@ -1,22 +1,26 @@
-// this solution is theoretically correct, but the numbers are too big to fit in an unsigned 128bit integer
 fn main() {
-    const GRID: u128 = 20;
-
-    println!("{}", (GRID * 2).factorial() / (GRID.factorial().pow(2)));
+    
 }
 
-trait Factorial {
-    fn factorial(&self) -> u128;
-}
-
-impl Factorial for u128 {
-    fn factorial(&self) -> u128 {
-        let mut factorial: u128 = 1;
-
-        for n in 1..=*self {
-            factorial *= n;
-        }
-
-        factorial
-    }
-}
+// this solution is theoretically correct, but the numbers are too big to fit in an unsigned 128bit integer
+// fn main() {
+//     const GRID: u128 = 20;
+// 
+//     println!("{}", (GRID * 2).factorial() / (GRID.factorial().pow(2)));
+// }
+// 
+// trait Factorial {
+//     fn factorial(&self) -> u128;
+// }
+// 
+// impl Factorial for u128 {
+//     fn factorial(&self) -> u128 {
+//         let mut factorial: u128 = 1;
+// 
+//         for n in 1..=*self {
+//             factorial *= n;
+//         }
+// 
+//         factorial
+//     }
+// }
