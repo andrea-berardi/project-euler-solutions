1ab705e244ee3b6daa9e16ceee5bf2ad20fd6f50
Andrea Berardi <Kotheos@protonmail.com>
2021-09-28 23:44:15 +0200

    this is slow but it works. the divisors function is bad

--- problems_21-30/problem_23/src/main.rs
+++ problems_21-30/problem_23/src/main.rs
@@ -1,35 +1,49 @@
 fn main() {
-    let mut total: u32 = 0;
+    const MAX: u32 = 28_123;
 
-    for n in 1..=28_123 {
-        total += n;
+    let mut abundants: Vec<u32> = Vec::with_capacity(MAX as usize);
+
+    for n in 1..=MAX {
+        if n.is_abundant() {
+            abundants.push(n);
+        }
+    }
+
+    let mut is_sum_of_abundants: Vec<bool> = vec![false; MAX as usize + 1];
+    for m in abundants.iter() {
+        for n in abundants.iter() {
+            if m + n <= MAX {
+                is_sum_of_abundants[(m + n) as usize] = true;
+            } else {
+                break;
+            }
+        }
+    }
+
+    let mut total: u32 = 0;
+    for i in is_sum_of_abundants.iter().enumerate() {
+        if *i.1 == false {
+            total += i.0 as u32;
+        }
     }
 
     println!("{}", total);
 }
 
 trait Divisors {
-    fn divisors_sum(&self) -> u32;
+    fn is_abundant(&self) -> bool;
 }
 
 impl Divisors for u32 {
-    fn divisors_sum(&self) -> u32 {
+    fn is_abundant(&self) -> bool {
         let mut divisors: Vec<u32> = Vec::new();
 
-        for i in 1..(*self as f32).sqrt() as u32 {
+        for i in 1..*self {
             if *self % i == 0 {
-                if *self / i == i {
-                    divisors.push(i);
-                } else {
-                    divisors.push(i);
-
-                    if *self != *self / i {
-                        divisors.push(*self / i);
-                    }
-                }
+                divisors.push(i);
             }
         }
 
-        divisors.iter().sum()
+        divisors.iter().sum::<u32>() > *self
     }
 }
