55bf5f4d9b38d5f7177ad96ab7ecf6ba30761883
Andrea Berardi <Kotheos@protonmail.com>
2021-10-01 11:23:31 +0200

    replaced while loop with a for loop

--- problems_21-30/problem_24/src/main.rs
+++ problems_21-30/problem_24/src/main.rs
@@ -5,9 +5,8 @@ fn main() {
     // thanks Dijkstra
     let mut permutation: Vec<u32> = (0..=9).collect();
     let p_len: usize = permutation.len();
-
-    let mut c: u32 = 1;
-    while c < 1_000_000 {
+    
+    for _ in 1..1_000_000 {
         let mut i: usize = p_len - 1;
         while permutation[i - 1] >= permutation[i] {
             i -= 1;
@@ -29,8 +28,6 @@ fn main() {
             i += 1;
             j -= 1;
         }
-
-        c += 1;
     }
 
     for n in permutation {
